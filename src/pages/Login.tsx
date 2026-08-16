21c5abdef551dbe7441bbce6c7257cd19290f92c
Kevin Gabriel <161509299+KevinSouza17@users.noreply.github.com>
2026-08-16 18:38:12 -0300

    Fix white screen on load

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Activity, Mail, Lock, Eye, EyeOff } from "lucide-react";
 import { Button } from "../components/ui/button";
